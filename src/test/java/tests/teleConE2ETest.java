9a3705d6b6a89ede91066dab7e6098fc27ff39a9
omkar-manastik <omkar@manastik.com>
2025-05-24 21:07:14 -0700

    Teleco flow: appointment flow

--- src/test/java/tests/teleConE2ETest.java
+++ src/test/java/tests/teleConE2ETest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
 import utils.TestUtils;
 
 public class teleConE2ETest extends BaseTest {
@@ -21,7 +22,48 @@ public class teleConE2ETest extends BaseTest {
 	private TestUtils utils;
 	@Test
 	public void medicoPositiveFlow() throws InterruptedException, MalformedURLException {
+
+		// User Phone
 		
+		userDriver.findElement(By.xpath(".//*[@content-desc='Next']")).click();
+		Thread.sleep(1000);
+		userDriver.findElement(By.xpath(".//*[@content-desc='Next']")).click();
+		userDriver.findElement(AppiumBy.accessibilityId("Get Started")).click();
+
+		userDriver.findElement(By.xpath("//android.view.View[@content-desc=\"English\n" + "A\"]")).click();
+		userDriver.findElement(AppiumBy.accessibilityId("Next")).click();
+
+		userDriver.findElement(AppiumBy.accessibilityId("User")).click();
+		userDriver.findElement(AppiumBy.accessibilityId("TEST")).click();
+		userDriver.findElement(AppiumBy.accessibilityId("PROD")).click();
+		Thread.sleep(1000);
+		userDriver.findElement(By.xpath("//android.widget.EditText[@hint='Email\nEmail Id']")).click();
+		userAction.sendKeys("omkar2chougale@gmail.com").perform();		
+		userDriver.hideKeyboard();
+		
+		userDriver.findElement(By.xpath("//android.widget.EditText[@hint='Password\nPassword']")).click();
+		userAction.sendKeys("Test@123").perform();
+		userDriver.hideKeyboard();
+		userDriver.findElement(AppiumBy.accessibilityId("Sign In")).click();
+		
+		utils.swipeUp(userDriver);
+		utils.scrollToTextUntilEnd(userDriver, "Consult our top experts");
+		userDriver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"View All\"]")).click();
+		
+		userDriver.findElement(By.xpath("(//android.view.View[@content-desc=\"Book\"])[1]")).click();
+		Thread.sleep(1000);
+		userDriver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.Button\").instance(1)")).click();
+		userDriver.findElement(AppiumBy.accessibilityId("26, Monday, May 26, 2025")).click();
+		userDriver.findElement(AppiumBy.accessibilityId("OK")).click();
+		userDriver.findElement(By.xpath("//android.view.View[@content-desc=\"Select the time slot\"]/following-sibling::android.view.View")).click();
+		
+		userDriver.findElement(AppiumBy.accessibilityId("Book")).click();	
+		Thread.sleep(1000);
+		userDriver.findElement(AppiumBy.accessibilityId("Book")).click();
+		userDriver.findElement(By.xpath("//android.widget.Button")).click();
+		
+		
+		// Medico phone
 		driver.findElement(By.xpath(".//*[@content-desc='Next']")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.xpath(".//*[@content-desc='Next']")).click();
@@ -42,44 +84,25 @@ public class teleConE2ETest extends BaseTest {
 		action.sendKeys("Test@456").perform();
 		driver.hideKeyboard();
 		driver.findElement(AppiumBy.accessibilityId("Sign In")).click();
+		
+		
+		driver.findElement(AppiumBy.accessibilityId("Pending appointment requests")).click();
+		driver.findElement(AppiumBy.accessibilityId("Accept")).click();
+		
 		driver.findElement(By.xpath("//android.view.View[@content-desc=\"View All\"]")).click();
+		
 		utils.scrollToTextUntilEnd(driver, "Automation user");
 		driver.findElement(By.xpath("//*[contains(@content-desc, 'Automation user')]")).click();
 		
+		driver.findElement(AppiumBy.accessibilityId("Start Appointment")).click();
+		
+		Thread.sleep(3000);
+	
 		// User Phone
-		setupUserDriver();
-		userDriver.findElement(By.xpath(".//*[@content-desc='Next']")).click();
-		Thread.sleep(1000);
-		userDriver.findElement(By.xpath(".//*[@content-desc='Next']")).click();
-		userDriver.findElement(AppiumBy.accessibilityId("Get Started")).click();
-
-		userDriver.findElement(By.xpath("//android.view.View[@content-desc=\"English\n" + "A\"]")).click();
-		userDriver.findElement(AppiumBy.accessibilityId("Next")).click();
-
-		userDriver.findElement(AppiumBy.accessibilityId("User")).click();
-		userDriver.findElement(AppiumBy.accessibilityId("TEST")).click();
-		userDriver.findElement(AppiumBy.accessibilityId("PROD")).click();
-		Thread.sleep(1000);
-		userDriver.findElement(By.xpath("//android.widget.EditText[@hint='Email\nEmail Id']")).click();
-		userAction.sendKeys("vishnu.kumar@manastik.com").perform();		
-		userDriver.hideKeyboard();
-		
-		userDriver.findElement(By.xpath("//android.widget.EditText[@hint='Password\nPassword']")).click();
-		userAction.sendKeys("Test@123").perform();
-		userDriver.hideKeyboard();
-		userDriver.findElement(AppiumBy.accessibilityId("Sign In")).click();
-		utils.swipeUp(userDriver);
-		utils.scrollToTextUntilEnd(userDriver, "Consult our top experts");
-		userDriver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"View All\"]")).click();
-		
-		userDriver.findElement(By.xpath("(//android.view.View[@content-desc=\"Book\"])[1]")).click();
-		Thread.sleep(1000);
-		userDriver.findElement(By.xpath("//android.view.View[@content-desc=\"Select the time slot\"]/following-sibling::android.view.View")).click();
-		
-		userDriver.findElement(AppiumBy.accessibilityId("Book")).click();	
-		Thread.sleep(1000);
-		userDriver.findElement(AppiumBy.accessibilityId("Book")).click();
-		
-		
+		 ((AndroidDriver) userDriver).openNotifications();
+		 userDriver.findElement(By.id("com.manastik.dadt:id/llAccept")).click();
+		 
+		// Medico Phone
+		 driver.findElement(AppiumBy.accessibilityId("Toggle Picture-in-Picture mode")).click();
 	}
 }
